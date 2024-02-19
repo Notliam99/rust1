e518ecd0021ccf2fa0cd6d144ca924ea3da4774c
Liam Tietejens <l.tietjens@hotmail.com>
2024-02-19 21:45:58 +1300

    wow even cooler alternitive screens

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,35 @@
+use crossterm::{
+    cursor::{DisableBlinking, EnableBlinking, MoveTo, RestorePosition, SavePosition},
+    execute,
+    terminal::{EnterAlternateScreen, LeaveAlternateScreen},
+};
 use std::io::{self, Write};
 
 fn main() {
-    let mut input = String::new();
+    execute!(io::stdout(), EnterAlternateScreen).expect("Error: Couldnt Enter Alternitive Screen");
+
+    let mut name = String::new();
 
     print!("hello whats your name\n> ");
 
     io::stdout().flush().unwrap();
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Error: Could Not Read Line");
-    print!("{esc}c", esc = 27 as char);
 
-    println!("Hello {input}");
+    execute!(
+        io::stdout(),
+        SavePosition,
+        MoveTo(10, 10),
+        EnableBlinking,
+        DisableBlinking,
+        RestorePosition
+    )
+    .expect("ERRor couldnt work");
+
+    io::stdin()
+        .read_line(&mut name)
+        .execute(MoveTo(11, 11))
+        .expect("Error: Could Not Read Line");
+
+    execute!(io::stdout(), LeaveAlternateScreen).expect("Error: Couldnt Exit Alternitive Screen");
+
+    println!("Hello {name}");
 }
